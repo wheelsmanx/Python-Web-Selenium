172257e0e311c0ccc48e34ea858b5601c5a6a47d
wheelsmanx <wheelsmanx@gmail.com>
2018-02-06 17:24:03 -0500

    updated selenium test
    updated selenium test

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,12 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+debug = False
+
+def fbLogin():
+    if(debug == True):
+        user = "YourTestEmail"
+        passWord = "YourTestPassWord"
+    else:
+        user = input("what is your userName?")
+        passWord = input("what is your passwWord")
+    driver = webdriver.Chrome()
+    driver.get("https://facebook.com")
+    assert "Facebook" in driver.title
+    elem = driver.find_element_by_id("email")
+    elem.clear()
+    elem.send_keys(user)
+    elem = driver.find_element_by_id("pass")
+    elem.clear()
+    elem.send_keys(passWord)
+    elem.send_keys(Keys.RETURN)
+    
+
+def getPosts():
+    elem = driver.find_element_by_id("p")
+    print(elem)
+
+
+
+fbLogin()
+getPosts()
